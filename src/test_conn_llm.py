ef78b81c47d9d31f887a572ada16276ee057680e
Eduardo Bezerra <edubezerra@gmail.com>
2025-02-20 15:42:31 -0300

    Change code to get keys from environment.

--- src/test_conn_llm.py
+++ src/test_conn_llm.py
@@ -2,8 +2,8 @@ import boto3
 import json
 import os
 
-AWS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_KEY = os.getenv("AWS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 
 if not AWS_KEY or not AWS_SECRET_KEY:
     raise ValueError("AWS credentials not found in environment variables")
