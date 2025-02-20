d20510727e96724b548a3954aadfc9264437f3c1
Eduardo Bezerra <edubezerra@gmail.com>
2025-02-20 18:25:48 -0300

    Update model_id

--- src/test_access_bedrock.py
+++ src/test_access_bedrock.py
@@ -1,7 +1,13 @@
 import boto3
 import json
 
+import os
 
+AWS_KEY = os.getenv("AWS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+
+if not AWS_KEY or not AWS_SECRET_KEY:
+    raise ValueError("AWS credentials not found in environment variables")
 
 bedrock_client = boto3.client(service_name='bedrock',
         region_name='us-east-1',
