398b682cc85bca3b11b4ee3cbd2e43d6e698f0ff
Eduardo Bezerra <edubezerra@gmail.com>
2025-02-20 18:39:34 -0300

    Add keys

--- src/test2_conn_llm.py
+++ src/test2_conn_llm.py
@@ -1,6 +1,13 @@
 import boto3
 import botocore.config
 import json
+import os
+
+AWS_KEY = os.getenv("AWS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+
+if not AWS_KEY or not AWS_SECRET_KEY:
+    raise ValueError("AWS credentials not found in environment variables")
 
 def blog_generate_using_bedrock(blogtopic:str)-> str:
     prompt=f"""<s>[INST]Human: Write a 200 words blog on the topic {blogtopic}
@@ -18,8 +25,11 @@ def blog_generate_using_bedrock(blogtopic:str)-> str:
         #model_id = "meta.llama3-3-70b-instruct-v1:0"
         model_id = "us.meta.llama3-1-70b-instruct-v1:0"
         model_id = "arn:aws:bedrock:us-east-1:816795924256:inference-profile/us.meta.llama3-1-70b-instruct-v1:0"
-        bedrock=boto3.client("bedrock-runtime",region_name="us-east-1",
-                             config=botocore.config.Config(read_timeout=300,retries={'max_attempts':3}))
+        bedrock=boto3.client("bedrock-runtime",
+                             region_name="us-east-1",
+                             config=botocore.config.Config(read_timeout=300,retries={'max_attempts':3}), 
+                             aws_access_key_id=AWS_KEY, 
+                             aws_secret_access_key=AWS_SECRET_KEY)
         response=bedrock.invoke_model(body=json.dumps(body),modelId=model_id)
 
         response_content=response.get('body').read()
